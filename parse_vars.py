831371fbdebcc0aa8d4d12f3a82471f35176af5f
youngkih <youngkih21@gmail.com>
2020-05-11 15:00:59 +0900

    LOCUST_FILE

--- parse_vars.py
+++ parse_vars.py
@@ -2,3 +2,6 @@ import os
 
 print("LOCUST_FILE : {}".format(os.getenv("LOCUST_FILE")))
 print("USER_COUNT : {}".format(os.getenv("USER_COUNT")))
+
+locust_files = os.getenv("LOCUST_FILE").split(",")
+user_counts= os.getenv("USER_COUNT").split(",")
